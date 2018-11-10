e6b1cb4a9c0b466e77ad5263b51440bf498a62cb
ilhamsj <ilhamsaputrajati@gmail.com>
2018-11-10 11:22:47 +0700

    periksa status response

--- public/project3/script.js
+++ public/project3/script.js
@@ -1,14 +1,35 @@
-var URL = "data/catatan.txt";
+var URL = "data/catatan.json";
+let t = document.getElementById("hasil");
 
-fetch(URL)
-    .then(function(response) {
-        if (response.status !== 200) {
-            console.log("ada masalah " + response.status);
-            return;
+// fetch(URL)
+//     .then(response => response.json())
+    
+//     .then(res => {
+//         t.textContent = res.judul + " di " + res.lokasi;
+//     })
+//     .catch(function(err) {
+//         console.log(err);
+//     });
+
+fetch("data/catatan.json")
+    .then(response => {
+        console.log(response.headers.get("Content-Type"));
+        console.log(response.headers.get("Date"));
+        console.log(response.status);
+        console.log(response.statusText);
+        console.log(response.type);
+        console.log(response.url);
+
+        if(response.ok) {
+            console.log("ok"); 
+            return response.text;
+        } else {
+            throw response.statusText;
         }
-        return response.text();
     })
-    .then(text => console.log(text))
-    .catch(err => console.log(err));
 
     //parameter response mempunyai nilai status (HTTP-Status) 
+    // cara menampilkan data
+    // menggunakan file json
+    // sederhanain kode biar enak dibaca
+    // gampang kan
