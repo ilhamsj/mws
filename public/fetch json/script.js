6eea80cad208068698944e36a2976dd0090d8fb0
ilhamsj <ilhamsaputrajati@gmail.com>
2018-11-10 19:15:04 +0700

    belajar fetch json

---
+++ public/fetch json/script.js
@@ -0,0 +1,45 @@
+var URL = "data/catatan.json";
+let t = document.getElementById("hasil");
+
+// fetch(URL)
+//     .then(response => response.json())
+    
+//     .then(res => {
+//         t.textContent = res.judul + " di " + res.lokasi;
+//     })
+//     .catch(function(err) {
+//         console.log(err);
+//     });
+
+// fetch("data/catatan.json")
+//     .then(response => {
+//         console.log(response.headers.get("Content-Type"));
+//         console.log(response.headers.get("Date"));
+//         console.log(response.status);
+//         console.log(response.statusText);
+//         console.log(response.type);
+//         console.log(response.url);
+
+//         if(response.ok) {
+//             console.log("ok"); 
+//             return response.text;
+//         } else {
+//             throw response.statusText;
+//         }
+//     })
+
+    //parameter response mempunyai nilai status (HTTP-Status) 
+    // cara menampilkan data
+    // menggunakan file json
+    // sederhanain kode biar enak dibaca
+    // gampang kan
+
+fetch(URL = "data/catatan.json")
+    .then(function(response) {
+        console.log(response)
+        if(response.ok) {
+            return response.text();
+        } else {
+            console.log(response.status)
+        }
+    })
